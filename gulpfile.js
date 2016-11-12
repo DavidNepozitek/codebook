db2fdf260da81c6496fcbd6ed77148d3f5499f57
David Nepožitek <david@nepozitek.cz>
2016-11-12 11:05:15 +0100

    Analytics changes

--- gulpfile.js
+++ gulpfile.js
@@ -96,9 +96,7 @@ gulp.task("js-back", function() {
             "assets/scripts/back/components/tags.js",
             "assets/scripts/back/components/navigation.js",
             "assets/scripts/back/components/tutorial.js",
-            "assets/scripts/back/components/activeusers.js",
-            "assets/scripts/back/components/daterange.js",
-            "assets/scripts/back/components/viewselector.js",
+            "assets/scripts/back/components/ga.js",
             "assets/scripts/back/components/onload.js"
         ])
         .pipe(plumber(plumberOpts))
